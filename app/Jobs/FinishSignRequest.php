ce98d61820b66903a59eca97d22f6571b5a95f66
Vadim Pronin <pupkin@afkxxx.com>
2017-12-31 15:52:47 +0200

    Implemented some Jobs

---
+++ app/Jobs/FinishSignRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace FakeIpastore\Jobs;
+
+use FakeIpastore\Models\SignRequest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class FinishSignRequest implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $signRequest;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param SignRequest $signRequest
+     */
+    public function __construct(SignRequest $signRequest)
+    {
+        $this->signRequest = $signRequest;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->signRequest->finishSignRequest();
+    }
+}
